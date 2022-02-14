c93a14ec6818d1591f39c00b541efbfb2e2b31a7
Alessandro <luisialessandro95@gmail.com>
2022-02-14 20:12:15 +0100

    attr old

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,31 +7,31 @@
         @csrf
         <div class="mb-3">
           <label for="title">Titolo Fumetto</label>
-          <input type="text" class="form-control" id="title" name="title">
+          <input type="text" class="form-control" id="title" name="title" value="{{old("title")}}">
         </div>
         <div class="mb-3">
             <label for="series">Serie</label>
-            <input type="text" class="form-control" id="series" name="series">
+            <input type="text" class="form-control" id="series" name="series" value="{{old("series")}}">
         </div>
         <div class="mb-3">
             <label for="type">Genere Letterario</label>
-            <input type="text" class="form-control" id="type" name="type">
+            <input type="text" class="form-control" id="type" name="type" value="{{old("type")}}">
         </div>
         <div class="mb-3">
             <label for="image">Copertina</label>
-            <input type="text" class="form-control" id="image" name="image" placeholder="url img">
+            <input type="text" class="form-control" id="image" name="image" placeholder="url img" value="{{old("image")}}">
         </div>      
         <div class="mb-3">
             <label for="price">Prezzo</label>
-            <input type="number" class="form-control" id="price" name="price">
+            <input type="number" class="form-control" id="price" name="price" value="{{old("price")}}">
         </div>
         <div class="mb-3">
             <label for="sale_date">Data</label>
-            <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="Y-m-g">
+            <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="YYYY-mm-gg" value="{{old("sale_date")}}">
         </div>
         <div class="form-floating">
             <label for="description"></label>
-            <textarea class="form-control" placeholder="Descrizione Fumetto" id="description" name="description" rows="8"></textarea>
+            <textarea class="form-control" placeholder="Descrizione Fumetto" id="description" name="description" rows="8" value="{{old("description")}}"></textarea>
         </div>
         <button type="submit" class="my-5 btn btn-info">Aggiungi</button>
         <a href="{{route('comics.index')}}"><button type="button" class="btn btn-primary">Back to Home</button></a>
